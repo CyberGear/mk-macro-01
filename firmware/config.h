8633df8bcb848da7960e211682a339995133d13d
Marius Kavoliunas <marius.kavoliunas@telesoftas.com>
2021-04-07 23:52:08 +0300

    implement initial macros firmware

--- firmware/config.h
+++ firmware/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2021 REPLACE_WITH_YOUR_NAME
+/* Copyright 2021 CyberGear Marius Kavoliunas
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -33,6 +33,3 @@
 
 #define DIODE_DIRECTION COL2ROW
 
-// #define TAP_CODE_DELAY 10
-
-// #define COMBO_COUNT 10
--- firmware/keymaps/CyberGear/keymap.c
+++ firmware/keymaps/CyberGear/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 REPLACE_WITH_YOUR_NAME
+/* Copyright 2021 CyberGear Marius Kavoliunas
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,43 +18,11 @@
 
 // https://codepen.io/mvaneijgen/full/LYEVQXp
 
-// enum custom_keycodes {
-//     MAC_1 = SAFE_RANGE,
-// };
-
-// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-//     switch (keycode) {
-//     case MAC_1:
-//         if (!record->event.pressed) {
-//             register_code16(KC_RCTRL);
-//             register_code16(KC_RGUI);
-//             register_code16(KC_RALT);
-//             tap_code16(KC_KP_1);
-//             unregister_code16(KC_RALT);
-//             unregister_code16(KC_RGUI);
-//             unregister_code16(KC_RCTRL);
-//         }
-//         break;
-//         return false;
-//     }
-//     return true;
-//
+#define MAC(kc) (QK_RCTL | QK_RGUI | QK_RALT | (kc))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_P1,  KC_P3,  KC_P3, KC_P4, KC_P5,
-        MO(1), MO(1), KC_P6, KC_P7, KC_P8
-    ),
-    [1] = LAYOUT(
-        RESET,   DEBUG,      KC_A, _______, _______,
-        _______, _______, _______, _______, _______
+        MAC(KC_KP_0), MAC(KC_KP_1), MAC(KC_KP_2), MAC(KC_KP_3), MAC(KC_KP_4),
+        MAC(KC_KP_5), MAC(KC_KP_6), MAC(KC_KP_7), MAC(KC_KP_8), MAC(KC_KP_9)
     )
 };
-
-// void keyboard_post_init_user(void) {
-//   // Customise these values to desired behaviour
-//   debug_enable=true;
-// //   debug_matrix=true;
-// //   debug_keyboard=true;
-//   //debug_mouse=true;
-// }
